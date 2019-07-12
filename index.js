de30e71d9f12a274dd4d0a61e8809ca07e11ba0e
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-07-12 21:47:31 +0300

    update code

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
-const csv = require('csv-parser');
 const mdbclient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/';
 const config = require('./config');
+const { exec } = require('child_process');
 
-const chunk = 100;
-const limit = 10000000;
-let curr = 0;
-let results = [];
 // connect to the server
 mdbclient.connect(url, {
   useNewUrlParser: true
@@ -24,35 +20,19 @@ mdbclient.connect(url, {
         conn.close();
         return console.error(err);
       }
-      col.createIndex({ Title: 1, Creator: 1, Publisher: 1 })
-      let stream = fs.createReadStream('./checkouts-by-title.csv')
-        .pipe(csv())
-        .on('data', (data) => {
-          if (curr >= limit) {
-            console.log('end');
-            stream.end();
-            return
-          }
-          results.push(data)
-          curr++;
-          if (curr % chunk === 0) {
-            if (results)
-              col.insertMany([...results], (err, res) => {
-                if (err) {
-                  console.error(err);
-                }
-              });
-            results = [];
-          }
-
-        })
-        .on('end', () => {
-          if (results)
-            col.insertMany(results, (err, res) => {
-              if (err) {
-                console.error(err);
-              }
-            });
-        });
+      col.createIndex({ Title: "text" });
+      col.createIndex({ Creator: 1 });
+      col.createIndex({ Publisher: 1 });
+      exec(`mongoimport --db ${config.db} --collection ${config.collection} --parseGrace skipRow --file import.csv --headerline`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          return;
+        }
+      
+        // the *entire* stdout and stderr (buffered)
+        console.log("finished!");
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
     });
 });
